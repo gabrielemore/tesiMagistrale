8b8edbe2602fe9c5298acbfa8f7f2eed929f48a7
Gabriele Morè <gabrymore@hotmail.com>
2024-11-19 12:26:46 +0100

    modifiche minori

--- matlab/model_identification_circadian_off.m
+++ matlab/model_identification_circadian_off.m
@@ -4,43 +4,47 @@ clc;
 close all;
 
 %% IMPORTAZIONE DATI
-data_circadian_off = load("C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Sim_gabriele_pasti_Circadian_off\Sim_gabriele_pasti_Circadian_off.mat");
+%data_circadian_off = load("C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Sim_gabriele_pasti_Circadian_off\Sim_gabriele_pasti_Circadian_off.mat");
 
+%messori
+data_circadian_off = load('C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Messori\TrainingMessori_circDisabled.mat');
 %% ESTRAZIONE DATI
+
+patient = 1;
 
 %time full simulation [min]
 time = data_circadian_off.SimSettings.simDurationMinutes; 
 
 %Gb - Glucose basal rate [mg/dl]
-Gb= data_circadian_off.Subjects(1).Params.Gb;
+Gb= data_circadian_off.Subjects(patient).Params.Gb;
 
 %CR - Ratio carbohydrate-to-insuline ratio [g/U]
-CR=data_circadian_off.Subjects(1).Params.CR;
+CR=data_circadian_off.Subjects(patient).Params.CR;
 
 %CF - Correction factor [mg/(dL·U)]
-CF=data_circadian_off.Subjects(1).Params.CF;
+CF=data_circadian_off.Subjects(patient).Params.CF;
 
 %Ub_day - daily basal insulin [U/day]
-Ub_day = data_circadian_off.Subjects(1).Params.dailyBasalInsulin;
+Ub_day = data_circadian_off.Subjects(patient).Params.dailyBasalInsulin;
 
 % u(t) - Input insuline [pmol]
-inputs_subqInsulin_Normal_Basal_Use= data_circadian_off.Subjects(1).Signals.inputs_subqInsulin_Normal_Basal_Use(2:end); 
-inputs_subqInsulin_Normal_Bolus_Use=data_circadian_off.Subjects(1).Signals.inputs_subqInsulin_Normal_Bolus_Use(2:end); 
+inputs_subqInsulin_Normal_Basal_Use= data_circadian_off.Subjects(patient).Signals.inputs_subqInsulin_Normal_Basal_Use(2:end); 
+inputs_subqInsulin_Normal_Bolus_Use=data_circadian_off.Subjects(patient).Signals.inputs_subqInsulin_Normal_Bolus_Use(2:end); 
 u = inputs_subqInsulin_Normal_Basal_Use + inputs_subqInsulin_Normal_Bolus_Use;
 
 %r(t) - rate of carbohydrate(CHO) intake [mg]
-r=data_circadian_off.Subjects(1).Signals.inputs_mealCHO(2:end); 
+r=data_circadian_off.Subjects(11).Signals.inputs_mealCHO(2:end); 
 
 %y(t) - sensor [mg/dl]
-y = data_circadian_off.Subjects(1).Signals.Sensors__replace_me__(2:end);
+y = data_circadian_off.Subjects(patient).Signals.Sensors__replace_me__(2:end);
 
 %IOB [U]
-IOB_b = data_circadian_off.Subjects(1).Signals.insulin_Normal_Basal_Use_IOB(2:end);
-IOB_bol = data_circadian_off.Subjects(1).Signals.insulin_Normal_Bolus_Use_IOB(2:end);
+IOB_b = data_circadian_off.Subjects(patient).Signals.insulin_Normal_Basal_Use_IOB(2:end);
+IOB_bol = data_circadian_off.Subjects(patient).Signals.insulin_Normal_Bolus_Use_IOB(2:end);
 IOB = IOB_b + IOB_bol;
 
 %Rate of glucose apperance in plasma [mg/kg/ min] 
-Ra = data_circadian_off.Subjects(1).Signals.subjE_G_app_rate(2:end); 
+Ra = data_circadian_off.Subjects(patient).Signals.subjE_G_app_rate(2:end); 
 
 %% CONVERSIONE UNITA DI MISURA
 % r - [mg]->[g]
@@ -67,8 +71,9 @@ o00 = o01*Gb + o02*Ub; % [mg/dL/min]
 theta0= [o00 o01 o02 o03 o04 o05]';
 
 %divisione dataset train e test
-T_4_days=60*24*4;
-T_3_days=time-T_4_days;
+%T_train=60*24*4;
+%diviosne dataset Messori
+T_train = 60*24*2;
 %definizione tempo di campionamento
 Ts = 1;
 %stati iniziali
@@ -85,7 +90,7 @@ options.StepTolerance = 1e-10;
 options.MaxFunctionEvaluations=100000;
 options.Display='iter-detailed';
 
-[theta_ott_ML] = fmincon(@(theta) cost_function(theta,x0,y,u,r,Ts,T_4_days,theta0),theta0, [], [], [], [], min, max, [],options);
+[theta_ott_ML] = fmincon(@(theta) cost_function(theta,x0,y,u,r,Ts,T_train,theta0),theta0, [], [], [], [], min, max, [],options);
 
 %% DEFINIZIONE SISTEMA LINEARE
 
@@ -110,31 +115,6 @@ disp("CR: " + CR + " - CR stimato: " + CR_cap);
 disp("theta4 iniziale: " + o04 + " - theta4 stimato: " + o4);
 disp("theta5 iniziale: " + o05 + " - theta5 stimato: " + o5);
 
-% %% SIMULAZIONE SISTEMA LINEARE CON PARAMETRI STIMATI
-% 
-% %train
-% x_train(:,1)=x0;
-% for k=1:T_4_days
-%     %sistema lineare
-%     x_train(:,k+1) = x_train(:,k) + Ts*(A*x_train(:,k) + B_u*u(k) + B_r*r(k) + E);
-%     y_cap_train(k)=C*x_train(:,k);
-% 
-%     %costruzione vettore medie al tempo k
-%     y_m_train(k)=mean(y(1:k));
-% end
-% 
-% %test
-% x_test(:,1)=x_train(:,T_4_days+1);
-% i=1;
-% for k=T_4_days+1:time
-%     %sistema lineare
-%     x_test(:,i+1) = x_test(:,i) + Ts*(A*x_test(:,i) + B_u*u(k) + B_r*r(k) + E);
-%     y_cap_test(i)=C*x_test(:,i);
-% 
-%     %costruzione vettore medie al tempo k
-%     y_m_test(i)=mean(y(T_4_days:k));
-%     i=i+1;
-% end
 %% SIMULAZIONE SISTEMA LINEARE CON PARAMETRI STIMATI
 
 x(:,1)=x0;
@@ -142,45 +122,44 @@ for k=1:time
     %sistema lineare
     x(:,k+1) = x(:,k) + Ts*(A*x(:,k) + B_u*u(k) + B_r*r(k) + E);
     y_cap(k)=C*x(:,k);
-    
 end
 
 %% METRICHE GoF TRAIN E TEST
 
 disp("-------------");
-GoF_train = 100*(1 - (norm(y(1:T_4_days)- y_cap(1:T_4_days)'))/(norm(y(1:T_4_days) - mean(y(1:T_4_days)))));
-GoF_test = 100*(1 - (norm(y(T_4_days+1:time)- y_cap(T_4_days+1:time)'))/(norm(y(T_4_days+1:time) - mean(y(T_4_days+1:time)))));
+GoF_train = 100*(1 - (norm(y(1:T_train)- y_cap(1:T_train)'))/(norm(y(1:T_train) - mean(y(1:T_train)))));
+GoF_test = 100*(1 - (norm(y(T_train+1:time)- y_cap(T_train+1:time)'))/(norm(y(T_train+1:time) - mean(y(T_train+1:time)))));
 disp("GoF train: " + GoF_train );
 disp("GoF test: " + GoF_test );
 
 %% CONFRONTO GRAFICO
 
 %-------------CGM--------------
-figure('Name', 'Confronto CGM: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto CGM: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, y(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Train)');
+plot(1:1:T_train, y(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Train)');
 hold on;
-plot(1:1:T_4_days, y_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Train)');
+plot(1:1:T_train, y_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, y(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Test)');
-plot(T_4_days+1:1:time, y_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Test)');
+plot(T_train+1:1:time, y(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Test)');
+plot(T_train+1:1:time, y_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('Glucosio [mg/dL]');
-title('Confronto CGM: Reale vs Stimato - Paziente 001');
+title(['Confronto CGM: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
@@ -189,31 +168,31 @@ set(gcf, 'Color', 'white');
 %calcolo IOB_cap
 IOB_cap = o4*(x(2,:) + x(3,:));
 
-figure('Name', 'Confronto IOB: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto IOB: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, IOB(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Train)');
+plot(1:1:T_train, IOB(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Train)');
 hold on;
-plot(1:1:T_4_days, IOB_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Train)');
+plot(1:1:T_train, IOB_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, IOB(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Test)');
-plot(T_4_days+1:1:time, IOB_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Test)');
+plot(T_train+1:1:time, IOB(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Test)');
+plot(T_train+1:1:time, IOB_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('IOB [U]');
-title('Confronto IOB: Reale vs Stimato - Paziente 001');
+title(['Confronto IOB: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
@@ -222,33 +201,34 @@ set(gcf, 'Color', 'white');
 %calcolo Ra_cap
 Ra_cap = o3*x(4,:);
 
-figure('Name', 'Confronto Ra: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto Ra: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, Ra(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Train)');
+plot(1:1:T_train, Ra(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Train)');
 hold on;
-plot(1:1:T_4_days, Ra_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Train)');
+plot(1:1:T_train, Ra_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, Ra(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Test)');
-plot(T_4_days+1:1:time, Ra_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Test)');
+plot(T_train+1:1:time, Ra(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Test)');
+plot(T_train+1:1:time, Ra_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('Ra [mg/(dl\cdotmin)]');
-title('Confronto Ra: Reale vs Stimato - Paziente 001');
+title(['Confronto Ra: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
 %% SALVATAGGIO PARAMETRI SISTEMA LINEARE CIRCADIAN OFF
 save('parametri_circadian_off.mat', 'theta_ott_ML');
+
--- matlab/model_identification_circadian_on.m
+++ matlab/model_identification_circadian_on.m
@@ -4,44 +4,44 @@ clc;
 close all;
 
 %% IMPORTAZIONE DATI
-data_circadian_off = load("C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Sim_gabriele_pasti_Circadian_on\Sim_gabriele_pasti_Circadian_on.mat");
+%data_circadian_off = load("C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Sim_gabriele_pasti_Circadian_on\Sim_gabriele_pasti_Circadian_on.mat");
 
+%messori
+data_circadian_off = load('C:\Users\ITAPC\Documents\università\tesi MAGISTRALE\dati\Messori\TrainingMessori_circEnabled.mat');
 %% ESTRAZIONE DATI
+
+patient = 1;
 
 %time full simulation [min]
 time = data_circadian_off.SimSettings.simDurationMinutes; 
 
 %Gb - Glucose basal rate [mg/dl]
-Gb= data_circadian_off.Subjects(1).Params.Gb;
-
-%CR - Ratio carbohydrate-to-insuline ratio [g/U]
-%CR=data_circadian_off.Subjects(1).Params.CR;
+Gb= data_circadian_off.Subjects(patient).Params.Gb;
 
 %CF - Correction factor [mg/(dL·U)]
-CF=data_circadian_off.Subjects(1).Params.CF;
+CF=data_circadian_off.Subjects(patient).Params.CF;
 
 %Ub_day - daily basal insulin [U/day]
-Ub_day = data_circadian_off.Subjects(1).Params.dailyBasalInsulin;
+Ub_day = data_circadian_off.Subjects(patient).Params.dailyBasalInsulin;
 
 % u(t) - Input insuline [pmol]
-inputs_subqInsulin_Normal_Basal_Use= data_circadian_off.Subjects(1).Signals.inputs_subqInsulin_Normal_Basal_Use(2:end); 
-inputs_subqInsulin_Normal_Bolus_Use=data_circadian_off.Subjects(1).Signals.inputs_subqInsulin_Normal_Bolus_Use(2:end); 
+inputs_subqInsulin_Normal_Basal_Use= data_circadian_off.Subjects(patient).Signals.inputs_subqInsulin_Normal_Basal_Use(2:end); 
+inputs_subqInsulin_Normal_Bolus_Use=data_circadian_off.Subjects(patient).Signals.inputs_subqInsulin_Normal_Bolus_Use(2:end); 
 u = inputs_subqInsulin_Normal_Basal_Use + inputs_subqInsulin_Normal_Bolus_Use;
 
 %r(t) - rate of carbohydrate(CHO) intake [mg]
-r=data_circadian_off.Subjects(1).Signals.inputs_mealCHO(2:end); 
+r=data_circadian_off.Subjects(11).Signals.inputs_mealCHO(2:end); 
 
 %y(t) - sensor [mg/dl]
-y = data_circadian_off.Subjects(1).Signals.Sensors__replace_me__(2:end);
+y = data_circadian_off.Subjects(patient).Signals.Sensors__replace_me__(2:end);
 
 %IOB [U]
-IOB_b = data_circadian_off.Subjects(1).Signals.insulin_Normal_Basal_Use_IOB(2:end);
-IOB_bol = data_circadian_off.Subjects(1).Signals.insulin_Normal_Bolus_Use_IOB(2:end);
+IOB_b = data_circadian_off.Subjects(patient).Signals.insulin_Normal_Basal_Use_IOB(2:end);
+IOB_bol = data_circadian_off.Subjects(patient).Signals.insulin_Normal_Bolus_Use_IOB(2:end);
 IOB = IOB_b + IOB_bol;
 
 %Rate of glucose apperance in plasma [mg/kg/ min] 
-Ra = data_circadian_off.Subjects(1).Signals.subjE_G_app_rate(2:end); 
-
+Ra = data_circadian_off.Subjects(patient).Signals.subjE_G_app_rate(2:end); 
 %% CONVERSIONE UNITA DI MISURA
 % r - [mg]->[g]
 r = r/1000;
@@ -72,8 +72,9 @@ o5=50; o6=25; o7=45; o8=1; o9=1;
 theta0= [o0 o1 o2 o3 o4 o5 o6 o7 o8 o9]';
 
 %divisione dataset train e test
-T_4_days=60*24*4;
-T_3_days=time-T_4_days;
+%T_train=60*24*4;
+%diviosne dataset Messori
+T_train = 60*24*3;
 %definizione tempo di campionamento
 Ts = 1;
 %stati iniziali sistema NON lineare (aggiunto nuovo stato Si)
@@ -90,7 +91,7 @@ options.StepTolerance = 1e-10;
 options.MaxFunctionEvaluations=100000;
 options.Display='iter-detailed';
 
-[theta_ott_NL] = fmincon(@(theta) cost_function_non_linear_system(theta,x0,y,u,r,Ts,T_4_days,theta0),theta0, [], [], [], [], lb, ub, [],options);
+[theta_ott_NL] = fmincon(@(theta) cost_function_non_linear_system(theta,x0,y,u,r,Ts,T_train,theta0),theta0, [], [], [], [], lb, ub, [],options);
 
 %% PARAMETRI OTTIMI SISTEMA NON LINEARE
 
@@ -146,39 +147,39 @@ end
 %% METRICHE GoF TRAIN E TEST
 
 disp("-------------");
-GoF_train = 100*(1 - (norm(y(1:T_4_days)- y_cap(1:T_4_days)))/(norm(y(1:T_4_days) - mean(y(1:T_4_days)))));
-GoF_test = 100*(1 - (norm(y(T_4_days+1:time)- y_cap(T_4_days+1:time)))/(norm(y(T_4_days+1:time) - mean(y(T_4_days+1:time)))));
+GoF_train = 100*(1 - (norm(y(1:T_train)- y_cap(1:T_train)))/(norm(y(1:T_train) - mean(y(1:T_train)))));
+GoF_test = 100*(1 - (norm(y(T_train+1:time)- y_cap(T_train+1:time)))/(norm(y(T_train+1:time) - mean(y(T_train+1:time)))));
 disp("GoF train: " + GoF_train );
 disp("GoF test: " + GoF_test );
 
 %% CONFRONTO GRAFICO
 
 %-------------CGM--------------
-figure('Name', 'Confronto CGM: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto CGM: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, y(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Train)');
+plot(1:1:T_train, y(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Train)');
 hold on;
-plot(1:1:T_4_days, y_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Train)');
+plot(1:1:T_train, y_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, y(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Test)');
-plot(T_4_days+1:1:time, y_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Test)');
+plot(T_train+1:1:time, y(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'CGM Reale (Test)');
+plot(T_train+1:1:time, y_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'CGM Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('Glucosio [mg/dL]');
-title('Confronto CGM: Reale vs Stimato - Paziente 001');
+title(['Confronto CGM: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
@@ -187,31 +188,31 @@ set(gcf, 'Color', 'white');
 %calcolo IOB_cap
 IOB_cap = o3*(x(2,:) + x(3,:));
 
-figure('Name', 'Confronto IOB: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto IOB: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, IOB(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Train)');
+plot(1:1:T_train, IOB(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Train)');
 hold on;
-plot(1:1:T_4_days, IOB_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Train)');
+plot(1:1:T_train, IOB_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, IOB(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Test)');
-plot(T_4_days+1:1:time, IOB_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Test)');
+plot(T_train+1:1:time, IOB(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'IOB Reale (Test)');
+plot(T_train+1:1:time, IOB_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'IOB Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('IOB [U]');
-title('Confronto IOB: Reale vs Stimato - Paziente 001');
+title(['Confronto IOB: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
@@ -220,31 +221,31 @@ set(gcf, 'Color', 'white');
 %calcolo Ra_cap
 Ra_cap = o2*x(4,:);
 
-figure('Name', 'Confronto Ra: Reale vs Stimato - Paziente 001');
+figure('Name', ['Confronto Ra: Reale vs Stimato - Paziente ' num2str(patient)]);
 % Plot dei dati 
 %train
-plot(1:1:T_4_days, Ra(1:T_4_days), 'r--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Train)');
+plot(1:1:T_train, Ra(1:T_train), 'r--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Train)');
 hold on;
-plot(1:1:T_4_days, Ra_cap(1:T_4_days), 'b-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Train)');
+plot(1:1:T_train, Ra_cap(1:T_train), 'b-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Train)');
 %test
-plot(T_4_days+1:1:time, Ra(T_4_days+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Test)');
-plot(T_4_days+1:1:time, Ra_cap(T_4_days+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Test)');
+plot(T_train+1:1:time, Ra(T_train+1:time), 'g--', 'LineWidth', 1.5, 'DisplayName', 'Ra Reale (Test)');
+plot(T_train+1:1:time, Ra_cap(T_train+1:time), 'm-', 'LineWidth', 1.5, 'DisplayName', 'Ra Stimato (Test)');
 
 % linea verticale di separazione
-xline(T_4_days, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
+xline(T_train, 'k--', 'LineWidth', 2, 'HandleVisibility', 'off');
 
 hold off;
 grid on;
-xlim([0, 10080]);
+xlim([0, time]);
 % Scritte test e train
 ylims = ylim; 
 y_pos = ylims(1) + 0.95*(ylims(2)-ylims(1));
-text(T_4_days/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
-text(T_4_days + T_3_days/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train/2, y_pos, 'Train','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
+text(T_train + (time - T_train)/2, y_pos, 'Test','HorizontalAlignment', 'center','FontSize', 14,'FontWeight', 'bold');
 
 xlabel('Tempo [min]');
 ylabel('Ra [mg/(dl\cdotmin)]');
-title('Confronto Ra: Reale vs Stimato - Paziente 001');
+title(['Confronto Ra: Reale vs Stimato - Paziente ' num2str(patient)]);
 legend('show');
 set(gca, 'FontSize', 12);
 set(gcf, 'Color', 'white');
